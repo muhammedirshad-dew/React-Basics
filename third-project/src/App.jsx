e5fad4822c7231ca9ee8d4884e0d4899c150ade7
Irshad-muhammed-me <irashimuhammed@gmail.com>
2025-04-10 15:25:52 +0530

    React-basics

---
+++ third-project/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Profile"
+        className="profile-pic"
+      />
+      <h1>Rahul</h1>
+      <p>Hi, I'm Rahul.</p>
+      <p>A passionate web developer who enjoys creating responsive websites.</p>
+      <p>Skilled in React, Node.js, and MongoDB.</p>
+    </div>
+  );
+}
+
+export default App;
